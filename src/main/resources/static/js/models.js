7af9cf6d5c3b57dc80076b0563510f9be9e02753
Matt Wolf <mwolf1963@gmail.com>
2021-02-16 18:31:22 -0500

    ready to test js

--- src/main/resources/static/js/models.js
+++ src/main/resources/static/js/models.js
@@ -4,8 +4,9 @@
 class Department{
 	constructor(){
 		let depNameSelector = document.getElementById("department_select_id");
-		this.department_id = depNameSelector[depNameSelector.selectedIndex].text;
+		this.department_id = depNameSelector[depNameSelector.selectedIndex].value;
 		this.name =  depNameSelector[depNameSelector.selectedIndex].text;
+		
 	}
 }
 class Customer{
@@ -20,12 +21,24 @@ class Part{
 	}
 }
 class Material{
-	
+	constructor(){
+		let matTypeSelector = document.getElementById("material_type_select_id");
+		this.id = matTypeSelector[matTypeSelector.selectedIndex].value;
+		this.materialType =  matTypeSelector[matTypeSelector.selectedIndex].text;
+	}
+}
+class ImprovementType{
+	constructor(){
+		let improvementTypeSelector = document.getElementById("improvement_type_select_id");
+		this.improvement_id = improvementTypeSelector[improvementTypeSelector.selectedIndex].value;
+		this.improvementType =  improvementTypeSelector[improvementTypeSelector.selectedIndex].text;
+	}
 }
 class Improvement {
 	constructor(){
 	this.department =  new Department();
 	this.customer = new Customer();
 	this.part = new Part();
+	this.improvementType = new ImprovementType();
 	}
 }
