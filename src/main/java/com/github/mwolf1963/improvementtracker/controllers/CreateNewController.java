ab4c113087a67d86a30a861c866c98b41a3deaa7
Matt <mwolf14@ivytech.edu>
2021-04-27 05:29:24 -0400

    working on an mvp

--- src/main/java/com/github/mwolf1963/improvementtracker/controllers/CreateNewController.java
+++ src/main/java/com/github/mwolf1963/improvementtracker/controllers/CreateNewController.java
@@ -1,12 +1,15 @@
 package com.github.mwolf1963.improvementtracker.controllers;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.github.mwolf1963.improvementtracker.models.Department;
 import com.github.mwolf1963.improvementtracker.models.Improvement;
@@ -22,16 +25,15 @@ import com.github.mwolf1963.improvementtracker.repositories.PartRepository;
 @Controller
 public class CreateNewController {
 	
-	@Autowired
-    private CustomerRepository customerRepository;
-	@Autowired
-    private PartRepository partRepository;
+	
 	@Autowired
 	private MaterialRepository materialRepository;
 	@Autowired
 	private DepartmentRepository departmentRepository;
 	@Autowired
 	private ImprovementTypeRepository improvementTypeRepository;
+	@Autowired
+	private ImprovementRepository improvementRepository;
 
 	public CreateNewController() {
 		// TODO Auto-generated constructor stub
@@ -47,10 +49,14 @@ public class CreateNewController {
 		model.addAttribute("departments", departments);
 		return "createnew";
 	}
-	@PostMapping("/createnewimprovement")
-	public String CreateNewImprovement() {
+	
+	//this needs fixed
+	@PostMapping(path="/createnewimprovement")
+	public String CreateNewImprovement( @RequestParam Map<String, String> newImprovement, Model model) {
+		Improvement improvement = new Improvement();
+		//get the data out of the Map and save it 
 		
-		
+		improvementRepository.saveAndFlush(improvement);
 		return "improvementcreated";
 	}
 
